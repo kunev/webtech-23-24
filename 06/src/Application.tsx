3ea439714d7b0de05ac17a6b01d239872c3ac18c
Evgeni Kunev <evgeni@kunev.me>
2024-04-06 22:28:58 +0300

    Render a basic board with hardcoded size

--- 06/src/Application.tsx
+++ 06/src/Application.tsx
@@ -1,7 +1,34 @@
-export function Application() {
+import {assert} from './lib'
+import {Game} from './minesweeper/game'
+
+import './style.css'
+
+type BoardProps = {
+  rows: number
+  columns: number
+  mines: number
+}
+
+function Board({rows, columns, mines}: BoardProps) {
+  const board = new Game(rows, columns, mines)
+
   return (
-    <div>
-      <h2>Hi! This is React</h2>
+    <div id="board">
+      {Array.from(Array(board.rows * board.columns)).map(() => (
+        <span className="cell"></span>
+      ))}
+    </div>
+  )
+}
+
+export function Application() {
+  const root = document.querySelector<HTMLElement>(':root')
+  assert(root)
+  root.style.setProperty('--board-size', '12')
+
+  return (
+    <div id="wrapper">
+      <Board rows={12} columns={12} mines={5} />
     </div>
   )
 }
